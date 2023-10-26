db60704bf6168008128e73de7b35ccbb0356f7c1
Keerthivasan Sekar <s.keerthivasan@kfinsolutions.com>
2023-10-26 13:26:03 +0530

    Initial commit

---
+++ app/src/main/java/com/ucal/tpms/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.ucal.tpms
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ucal.tpms.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
