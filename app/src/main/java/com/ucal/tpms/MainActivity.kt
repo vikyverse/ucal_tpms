3b5d1e1722aefbe31ad7ddbed16560c5e3bc1ba9
Keerthivasan Sekar <s.keerthivasan@kfinsolutions.com>
2023-10-26 17:41:54 +0530

    Scanning module complete

--- app/src/main/java/com/ucal/tpms/MainActivity.kt
+++ app/src/main/java/com/ucal/tpms/MainActivity.kt
@@ -1,15 +1,132 @@
 package com.ucal.tpms
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.bluetooth.BluetoothManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.lorenzofelletti.permissions.BuildConfig.DEBUG
+import com.lorenzofelletti.permissions.PermissionManager
+import com.lorenzofelletti.permissions.dispatcher.dsl.checkPermissions
+import com.lorenzofelletti.permissions.dispatcher.dsl.doOnDenied
+import com.lorenzofelletti.permissions.dispatcher.dsl.doOnGranted
+import com.lorenzofelletti.permissions.dispatcher.dsl.showRationaleDialog
+import com.lorenzofelletti.permissions.dispatcher.dsl.withRequestCode
+import com.ucal.tpms.blescanner.BleScanManager
+import com.ucal.tpms.blescanner.adapter.BleDeviceAdapter
+import com.ucal.tpms.blescanner.model.BleDevice
+import com.ucal.tpms.blescanner.model.BleScanCallback
 import com.ucal.tpms.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
+    private lateinit var btnStartScan: Button
+
+    private lateinit var permissionManager: PermissionManager
+
+    private lateinit var btManager: BluetoothManager
+    private lateinit var bleScanManager: BleScanManager
+
+    private lateinit var foundDevices: MutableList<BleDevice>
+
+    @SuppressLint("MissingPermission")
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        permissionManager = PermissionManager(this)
+        permissionManager buildRequestResultsDispatcher {
+            withRequestCode(BLE_PERMISSION_REQUEST_CODE) {
+                checkPermissions(blePermissions)
+                showRationaleDialog(getString(R.string.ble_permission_rationale))
+                doOnGranted { bleScanManager.scanBleDevices() }
+                doOnDenied {
+                    Toast.makeText(
+                        this@MainActivity,
+                        getString(R.string.ble_permissions_denied_message),
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+
+        // RecyclerView handling
+        val rvFoundDevices = findViewById<View>(R.id.rvDeviceList) as RecyclerView
+        foundDevices = BleDevice.createBleDevicesList()
+        val adapter = BleDeviceAdapter(foundDevices)
+        rvFoundDevices.adapter = adapter
+        rvFoundDevices.layoutManager = LinearLayoutManager(this)
+
+        // BleManager creation
+        btManager = getSystemService(BluetoothManager::class.java)
+        bleScanManager = BleScanManager(btManager, 5000, scanCallback = BleScanCallback({
+            val name = it?.device?.name
+            if (name != null) {
+                Log.d("Info", name)
+            }
+            if (name.isNullOrBlank()) return@BleScanCallback
+
+            val device = BleDevice(name)
+            if (!foundDevices.contains(device)) {
+                if (DEBUG) {
+                    Log.d(
+                        BleScanCallback::class.java.simpleName,
+                        "${this.javaClass.enclosingMethod?.name} - Found device: $name"
+                    )
+                }
+                foundDevices.add(device)
+                adapter.notifyItemInserted(foundDevices.size - 1)
+            }
+        }))
+
+        // Adding the actions the manager must do before and after scanning
+        bleScanManager.beforeScanActions.add { btnStartScan.isEnabled = false }
+        bleScanManager.beforeScanActions.add {
+            foundDevices.size.let {
+                foundDevices.clear()
+                adapter.notifyItemRangeRemoved(0, it)
+            }
+        }
+        bleScanManager.afterScanActions.add { btnStartScan.isEnabled = true }
+
+        // Adding the onclick listener to the start scan button
+        btnStartScan = findViewById(R.id.btnStartScan)
+        btnStartScan.setOnClickListener {
+            if (DEBUG) Log.i(TAG, "${it.javaClass.simpleName}:${it.id} - onClick event")
+
+            // Checks if the required permissions are granted and starts the scan if so, otherwise it requests them
+            permissionManager checkRequestAndDispatch BLE_PERMISSION_REQUEST_CODE
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionManager.dispatchOnRequestPermissionsResult(requestCode, grantResults)
+    }
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+
+        private const val BLE_PERMISSION_REQUEST_CODE = 1
+        @RequiresApi(Build.VERSION_CODES.S)
+        private val blePermissions = arrayOf(
+            Manifest.permission.BLUETOOTH_SCAN,
+            Manifest.permission.BLUETOOTH_CONNECT,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.BLUETOOTH_ADMIN,
+        )
     }
 }
---
+++ app/src/main/java/com/ucal/tpms/blescanner/adapter/BleDeviceAdapter.kt
@@ -0,0 +1,32 @@
+package com.ucal.tpms.blescanner.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ucal.tpms.R
+import com.ucal.tpms.blescanner.model.BleDevice
+
+class BleDeviceAdapter(private val devices: List<BleDevice>) : RecyclerView.Adapter<BleDeviceAdapter.ViewHolder>() {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val deviceNameTextView: TextView = itemView.findViewById(R.id.device_name)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        val deviceView = inflater.inflate(R.layout.device_row_layout, parent, false)
+        return ViewHolder(deviceView)
+    }
+
+    override fun onBindViewHolder(holder: BleDeviceAdapter.ViewHolder, position: Int) {
+        val device = devices[position]
+        val textView = holder.deviceNameTextView
+        textView.text = device.name
+    }
+
+    override fun getItemCount(): Int {
+        return devices.size
+    }
+}
